8a003ba9b00bb822951793506edde426383289b1
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-23 05:26:24 +0100

    Created theme project and set readme & manifest.

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Lombiq Orchard Core Base Theme",
+    Author = "Lombiq Technologies",
+    Version = "1.0",
+    Website = "https://github.com/Lombiq/Orchard-Base-Theme",
+    Description = "The base frontend theme for shared content that are not specific to a specific project's theme."
+)]
