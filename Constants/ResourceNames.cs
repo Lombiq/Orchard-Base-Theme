5a24bcde49b724d196f4308959e8cd830679cda7
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-03 07:43:34 +0100

    Include page style in layout.

---
+++ Constants/ResourceNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Lombiq.BaseTheme.Constants
+{
+    public static class ResourceNames
+    {
+        private const string Prefix = "Lombiq.BaseTheme.";
+
+        public const string Site = Prefix + nameof(Site);
+    }
+}
