8865b11077225db3f2efc7b95bd3e55977209dd0
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-05 11:53:48 +0100

    Use zone name constants.

---
+++ Constants/ZoneNames.cs
@@ -0,0 +1,18 @@
+﻿namespace Lombiq.BaseTheme.Constants
+{
+    public static class ZoneNames
+    {
+        public const string Header = nameof(Header);
+        public const string Navigation = nameof(Navigation);
+        public const string BeforeMain = nameof(BeforeMain);
+        public const string Featured = nameof(Featured);
+        public const string AsideFirst = nameof(AsideFirst);
+        public const string Messages = nameof(Messages);
+        public const string BeforeContent = nameof(BeforeContent);
+        public const string Content = nameof(Content);
+        public const string AfterContent = nameof(AfterContent);
+        public const string AsideSecond = nameof(AsideSecond);
+        public const string AfterMain = nameof(AfterMain);
+        public const string Footer = nameof(Footer);
+    }
+}
