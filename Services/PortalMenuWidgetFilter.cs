b24b780ab738e42934cdda4185426b5a2e1e46da
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-05 13:10:24 +0100

    Apply NoWrapper option and inject zone classes.

--- Services/PortalMenuWidgetFilter.cs
+++ Services/PortalMenuWidgetFilter.cs
@@ -1,5 +1,4 @@
-﻿using Lombiq.BaseTheme.Constants;
-using Lombiq.HelpfulExtensions.Extensions.Widgets;
+﻿using Lombiq.HelpfulExtensions.Extensions.Widgets;
 using Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels;
 using Lombiq.HelpfulLibraries.Libraries.Mvc;
 using Microsoft.AspNetCore.Authorization;
@@ -9,6 +8,7 @@ using OrchardCore.DisplayManagement.Layout;
 using OrchardCore.Navigation;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using static Lombiq.BaseTheme.Constants.ZoneNames;
 
 namespace Lombiq.BaseTheme.Services
 {
@@ -17,8 +17,9 @@ namespace Lombiq.BaseTheme.Services
         private readonly IAuthorizationService _authorizationService;
         private readonly IEnumerable<INavigationProvider> _navigationProviders;
         private readonly IHttpContextAccessor _hca;
+        private readonly ICssClassHolder _cssClassHolder;
 
-        protected override string ZoneName => ZoneNames.Navigation;
+        protected override string ZoneName => Navigation;
         protected override string ViewName => WidgetTypes.PortalMenuWidget;
         protected override bool AdminOnly => false;
 
@@ -27,21 +28,30 @@ namespace Lombiq.BaseTheme.Services
             ILayoutAccessor layoutAccessor,
             IShapeFactory shapeFactory,
             IEnumerable<INavigationProvider> navigationProviders,
-            IHttpContextAccessor hca)
+            IHttpContextAccessor hca,
+            ICssClassHolder cssClassHolder)
             : base(requiredPermission: null, authorizationService, layoutAccessor, shapeFactory)
         {
             _authorizationService = authorizationService;
             _navigationProviders = navigationProviders;
             _hca = hca;
+            _cssClassHolder = cssClassHolder;
         }
 
-        protected override async Task<PortalMenuWidgetViewModel> GetViewModelAsync() =>
-            new()
+        protected override async Task<PortalMenuWidgetViewModel> GetViewModelAsync()
+        {
+            // Add the <nav> classes to the zone holder <nav>.
+            _cssClassHolder.AddClassToZone(Navigation, "navbar-expand-md");
+            _cssClassHolder.AddClassToZone(Navigation, "navbar");
+
+            return new()
             {
                 MenuItems = await PortalMenuWidgetDisplayDriver.GetMenuItemsAsync(
                     _authorizationService,
                     _navigationProviders,
                     _hca),
+                NoWrapper = true, // The navigation zone is already the wrapper.
             };
+        }
     }
 }
