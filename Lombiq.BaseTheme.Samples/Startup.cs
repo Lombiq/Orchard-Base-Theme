bee7cb08487e308562de72d4af05b525d36453a3
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-15 18:02:56 +0200

    Add sample navigation provider.

--- Lombiq.BaseTheme.Samples/Startup.cs
+++ Lombiq.BaseTheme.Samples/Startup.cs
@@ -1,12 +1,17 @@
+using Lombiq.DataTables.Samples.Navigation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
+using OrchardCore.Navigation;
 using OrchardCore.ResourceManagement;
 
 namespace Lombiq.BaseTheme.Samples;
 
 public class Startup : StartupBase
 {
-    public override void ConfigureServices(IServiceCollection services) =>
+    public override void ConfigureServices(IServiceCollection services)
+    {
         services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+        services.AddScoped<INavigationProvider, AccountNavigationProvider>();
+    }
 }
