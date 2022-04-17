742f6e4fd2f20be528f07ea82f311d6062ff59ce
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-18 01:54:52 +0200

    Add tests and fix problems.

---
+++ Lombiq.BaseTheme.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
+
+namespace Lombiq.BaseTheme.Tests.UI.Extensions;
+
+public static class TestCaseUITestContextExtensions
+{
+    public static Task TestBaseThemeFeaturesAsync(this UITestContext context)
+    {
+        context.Get(By.Id("footer")).GetAttribute("class").Split().ShouldContain("text-center");
+        context.TestZoneInsertion();
+        return context.TestMainMenuWithAuthenticationAsync();
+    }
+
+    public static void TestZoneInsertion(this UITestContext context)
+    {
+        context.Get(By.ClassName("zoneInsertionExample_header"))
+            .Text
+            .Trim()
+            .ShouldBe("Here you can easily inject shapes into the zones, kind of like always enabled widgets.");
+
+        context.GetAllWhenOneExists(By.ClassName("zoneInsertionExample_footer"))
+            .Count
+            .ShouldBe(2);
+    }
+
+    public static async Task TestMainMenuWithAuthenticationAsync(this UITestContext context)
+    {
+        var navigationState = context.AsPageNavigationState();
+        await context.ClickMainMenuPathAsync("Account", "Admin");
+        navigationState.Wait();
+        new Uri(context.Driver.Url).AbsolutePath.ShouldBe("/Admin");
+
+        await context.GoToHomePageAsync();
+        await context.ClickMainMenuPathAsync("Account", "Log Out");
+
+        await context.ClickMainMenuPathAsync("Log In");
+        context.Exists(By.XPath("//form[@action = '/Login']/h4[contains(., 'Log in')]"));
+    }
+}
---
+++ Lombiq.BaseTheme.Tests.UI/Extensions/UITestContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Services;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using System.Threading.Tasks;
+
+namespace Lombiq.BaseTheme.Tests.UI.Extensions;
+
+public static class UITestContextExtensions
+{
+    public static async Task ClickMainMenuPathAsync(this UITestContext context, string topMenuLabel, string subMenuLabel = null)
+    {
+        var byFirst = By.XPath(
+            $"//div[contains(@class, \"menuWidget__content\")]/ul/li/a" +
+            $"[contains(@class, \"nav-link\") and contains(., {JsonConvert.SerializeObject(topMenuLabel)})]");
+
+        if (string.IsNullOrWhiteSpace(subMenuLabel))
+        {
+            await context.ClickReliablyOnAsync(byFirst);
+        }
+        else
+        {
+            await context.SelectFromBootstrapDropdownReliablyAsync(byFirst, subMenuLabel);
+        }
+    }
+}
