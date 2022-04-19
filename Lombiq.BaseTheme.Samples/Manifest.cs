2afb7c3287285a3c459c60e07766323074741c04
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-19 03:58:06 +0200

    Elaborate in manifest.

--- Lombiq.BaseTheme.Samples/Manifest.cs
+++ Lombiq.BaseTheme.Samples/Manifest.cs
@@ -10,6 +10,9 @@ using static Lombiq.BaseTheme.Constants.FeatureIds;
     Version = "1.0",
     Website = "https://github.com/Lombiq/Orchard-Base-Theme",
     Description = "A sample theme that builds on Lombiq Base Theme.",
+    // A base theme is another theme project. Orchard Core Display Management first searches your theme and then the
+    // base theme for template alternates. Besides that it's like similar to a dependency in modules, so any services
+    // registered in the base theme are also accessible.
     BaseTheme = BaseTheme
 )]
 
