9ee2f857e32d8d4931d55c3a3d7ad80a75524e4a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-05-06 00:31:23 +0200

    Updating version numbers

--- Lombiq.BaseTheme.Samples/Manifest.cs
+++ Lombiq.BaseTheme.Samples/Manifest.cs
@@ -7,7 +7,7 @@ using static Lombiq.BaseTheme.Constants.FeatureIds;
 [assembly: Theme(
     Name = "Lombiq Base Theme - Samples",
     Author = "Lombiq Technologies",
-    Version = "1.0.0-alpha",
+    Version = "1.0.0",
     Website = "https://github.com/Lombiq/Orchard-Base-Theme",
     Description = "A sample theme that builds on Lombiq Base Theme.",
     // A base theme is another theme project. Orchard Core Display Management first searches your theme and then the
--- Lombiq.BaseTheme/Manifest.cs
+++ Lombiq.BaseTheme/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "Lombiq Base Theme",
     Author = "Lombiq Technologies",
-    Version = "1.0.0-alpha",
+    Version = "1.0.0",
     Website = "https://github.com/Lombiq/Orchard-Base-Theme",
     Description = "The base frontend theme for shared content that are not specific to a specific project's theme."
 )]
