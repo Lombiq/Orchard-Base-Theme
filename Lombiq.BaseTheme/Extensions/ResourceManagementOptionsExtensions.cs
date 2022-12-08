9821b1ae7ea91505990546c912a7ffb01a3db409
Dávid El-Saig <david.el-saig@lombiq.com>
2022-12-08 17:34:21 +0100

    Rewrite RemoveBootstrap5 to only lock when there is actually anything to remove.
    (now this only actually locks once)

--- Lombiq.BaseTheme/Extensions/ResourceManagementOptionsExtensions.cs
+++ Lombiq.BaseTheme/Extensions/ResourceManagementOptionsExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Lombiq.BaseTheme.Constants;
 using OrchardCore.ResourceManagement;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.Extensions.Options;
 
@@ -7,21 +9,26 @@ public static class ResourceManagementOptionsExtensions
 {
     private static readonly object _lock = new();
 
+    private static readonly string[] _resourceTypes = { "stylesheet", "script" };
+
     public static void RemoveBootstrap5(this IOptions<ResourceManagementOptions> resourceManagementOptions)
     {
+        var resourcesToClear = resourceManagementOptions
+            .Value
+            .ResourceManifests
+            .Where(manifest => !manifest.GetResources("$" + nameof(FeatureIds.Area)).ContainsKey(FeatureIds.Area))
+            .SelectMany(manifest => _resourceTypes
+                .SelectWhere(manifest.GetResources)
+                .SelectWhere(resources => resources.GetMaybe("bootstrap"), resource => resource?.Any() == true))
+            .ToList();
+
+        if (!resourcesToClear.Any()) return;
+
         lock (_lock)
         {
-            foreach (var manifest in resourceManagementOptions.Value.ResourceManifests)
+            foreach (var resource in resourcesToClear)
             {
-                if (manifest.GetResources("$" + nameof(FeatureIds.Area)).ContainsKey(FeatureIds.Area)) continue;
-
-                foreach (var resourceType in new[] { "stylesheet", "script" })
-                {
-                    if (manifest.GetResources(resourceType).TryGetValue("bootstrap", out var resource))
-                    {
-                        resource.Clear();
-                    }
-                }
+                resource.Clear();
             }
         }
     }
