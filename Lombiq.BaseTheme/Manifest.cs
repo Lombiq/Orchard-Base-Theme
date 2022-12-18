5d4f6d0560c92f0fa3034e9083d90c050dbe4bb5
Dávid El-Saig <david.el-saig@lombiq.com>
2022-12-18 11:29:30 +0100

    Add notes about tenants.

--- Lombiq.BaseTheme/Manifest.cs
+++ Lombiq.BaseTheme/Manifest.cs
@@ -5,5 +5,7 @@ using OrchardCore.DisplayManagement.Manifest;
     Author = "Lombiq Technologies",
     Version = "0.0.1",
     Website = "https://github.com/Lombiq/Orchard-Base-Theme",
-    Description = "The base frontend theme for shared content that are not specific to a specific project's theme."
+    Description = "The base frontend theme for shared content that are not specific to a specific project's theme." +
+      "Warning: themes using this as the base remove the stock Bootstrap resource. If you switch to a different " +
+      "theme, please reload the tenant from Configuration > Tenants in the admin menu."
 )]
