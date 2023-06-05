4a62a5e18d86b2d6b6026074a845e437a37778fd
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-06-05 17:04:09 +0200

    DRY-ing string.Create(CultureInfo.InvariantCulture, ...)) calls

--- Lombiq.BaseTheme/Models/ZoneDescriptor.cs
+++ Lombiq.BaseTheme/Models/ZoneDescriptor.cs
@@ -1,11 +1,11 @@
 using GraphQL;
 using Lombiq.BaseTheme.Constants;
 using Lombiq.BaseTheme.Services;
+using Lombiq.HelpfulLibraries.Common.Utilities;
 using Microsoft.AspNetCore.Html;
 using OrchardCore.DisplayManagement.Razor;
 using OrchardCore.DisplayManagement.Zones;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -47,7 +47,7 @@ public class ZoneDescriptor
         var id = ZoneName.ToCamelCase();
         var layoutClassName = string.IsNullOrEmpty(parent)
             ? "layout" + ZoneName
-            : string.Create(CultureInfo.InvariantCulture, $"layout{parent}__{id}");
+            : StringHelper.CreateInvariant($"layout{parent}__{id}");
 
         var classNames = classHolder.ConcatenateZoneClasses(
             ZoneName,
@@ -69,17 +69,17 @@ public class ZoneDescriptor
             var elementName = ZoneName == ZoneNames.Content ? "main" : "div";
 
             body = new HtmlContentBuilder()
-                .AppendHtml(string.Create(CultureInfo.InvariantCulture, $"<{elementName} class=\"{bodyWrapperClass} {LeafClassName}\">"))
+                .AppendHtml(StringHelper.CreateInvariant($"<{elementName} class=\"{bodyWrapperClass} {LeafClassName}\">"))
                 .AppendHtml(body)
-                .AppendHtml(string.Create(CultureInfo.InvariantCulture, $"</{elementName}>"));
+                .AppendHtml(StringHelper.CreateInvariant($"</{elementName}>"));
         }
 
         return new HtmlContentBuilder()
-            .AppendHtml(string.Create(CultureInfo.InvariantCulture, $"<{ElementName} id=\"{id}\" class=\"{classNames}\">"))
+            .AppendHtml(StringHelper.CreateInvariant($"<{ElementName} id=\"{id}\" class=\"{classNames}\">"))
             .AppendHtml(await ConcatenateAsync(classHolder, page, ChildrenBefore, parent))
             .AppendHtml(body)
             .AppendHtml(await ConcatenateAsync(classHolder, page, ChildrenAfter, parent))
-            .AppendHtml(string.Create(CultureInfo.InvariantCulture, $"</{ElementName}>"));
+            .AppendHtml(StringHelper.CreateInvariant($"</{ElementName}>"));
     }
 
     private Task<IHtmlContent> ConcatenateAsync<TModel>(
@@ -102,7 +102,7 @@ public class ZoneDescriptor
 
         var newParent = string.IsNullOrEmpty(parent)
             ? zoneName
-            : string.Create(CultureInfo.InvariantCulture, $"{parent}__{zoneName}");
+            : StringHelper.CreateInvariant($"{parent}__{zoneName}");
 
         foreach (var zoneDescriptor in zoneDescriptors)
         {
