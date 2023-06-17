0e64106b89c5dba041d1bb026c4e48c4997a347e
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-17 22:59:41 +0200

    Code cleanup.

--- Lombiq.BaseTheme.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.BaseTheme.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -44,10 +44,42 @@ public static class TestCaseUITestContextExtensions
         context.Exists(By.XPath("//form[@action = '/Login']/*[starts-with(name(), 'h') and contains(., 'Log in')]"));
     }
 
-    public static async Task BaseThemeDependencyShouldBeEnabledAsync(this UITestContext context)
+    public static async Task TestBaseThemeDependencyIsEnabledAsync(this UITestContext context)
     {
         await context.GoToAdminRelativeUrlAsync("/Features");
-        await context.ClickAndFillInWithRetriesAsync(By.Id("search-box"), "Helpful Widgets");
         context.Exists(By.Id("btn-disable-Lombiq_HelpfulExtensions_Widgets"));
+    }
+
+    public static void TestBlogRecipeMenuItemsAddedToMainMenu(this UITestContext context)
+    {
+        context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/']")).Text.Trim().ShouldBe("Home");
+        context.Get(By.CssSelector(".menuWidget__content .nav-link[href='/about']")).Text.Trim().ShouldBe("About");
+    }
+
+    public static async Task TestAddingMenuItemToBlogMainMenu(this UITestContext context)
+    {
+        await context.GoToAdminRelativeUrlAsync("/Contents/ContentItems/Menu");
+        await context.ClickReliablyOnAsync(By.ClassName("edit"));
+
+        await context.ClickReliablyOnAsync(By.XPath("//button[contains(., 'Add Menu Item')]"));
+        await context.ClickReliablyOnAsync(By.XPath(
+            "//div[contains(@class, 'card') and .//h4[contains(., 'Content Menu Item')]]//div[contains(@class, 'card-footer')]//a"));
+        await context.ClickAndFillInWithRetriesAsync(By.Id("ContentMenuItemPart_Name"), "My Content");
+
+        await context.SetContentPickerByDisplayTextAsync(
+            "ContentMenuItemPart",
+            "SelectedContentItem",
+            "Man must explore, and this is exploration at its greatest");
+
+        await context.ClickPublishAsync();
+        await context.ClickPublishAsync();
+        context.ShouldBeSuccess();
+
+        await context.GoToHomePageAsync();
+        context
+            .Get(By.XPath("id('navigation')//li[contains(@class, 'menuWidget__topLevel')]/a[@href='/blog/post-1']"))
+            .Text
+            .Trim()
+            .ShouldBe("My Content");
     }
 }
