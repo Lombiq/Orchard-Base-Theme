5e8454dd809a30e77c864b4699b94d16e84e2513
Dávid El-Saig <david.el-saig@lombiq.com>
2023-07-03 18:54:26 +0200

    Don't build menu if the site setting is disabled.

--- Lombiq.BaseTheme/Services/MainMenuWidgetFilter.cs
+++ Lombiq.BaseTheme/Services/MainMenuWidgetFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Lombiq.BaseTheme.Constants;
+using Lombiq.BaseTheme.Models;
 using Lombiq.HelpfulExtensions.Extensions.Widgets;
 using Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels;
 using Lombiq.HelpfulLibraries.OrchardCore.Mvc;
@@ -7,7 +8,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using OrchardCore.DisplayManagement;
 using OrchardCore.DisplayManagement.Layout;
+using OrchardCore.Entities;
 using OrchardCore.Navigation;
+using OrchardCore.Settings;
 using System.Threading.Tasks;
 
 namespace Lombiq.BaseTheme.Services;
@@ -17,6 +20,7 @@ public class MainMenuWidgetFilter : WidgetFilterBase<MenuWidgetViewModel>
     private readonly INavigationManager _navigationManager;
     private readonly IActionContextAccessor _actionContextAccessor;
     private readonly ICssClassHolder _cssClassHolder;
+    private readonly ISiteService _siteService;
 
     protected override string ZoneName => ZoneNames.Navigation;
     protected override string ViewName => WidgetTypes.MenuWidget;
@@ -28,16 +32,21 @@ public class MainMenuWidgetFilter : WidgetFilterBase<MenuWidgetViewModel>
         IShapeFactory shapeFactory,
         INavigationManager navigationManager,
         IActionContextAccessor actionContextAccessor,
-        ICssClassHolder cssClassHolder)
+        ICssClassHolder cssClassHolder,
+        ISiteService siteService)
         : base(requiredPermission: null, authorizationService, layoutAccessor, shapeFactory)
     {
         _navigationManager = navigationManager;
         _actionContextAccessor = actionContextAccessor;
         _cssClassHolder = cssClassHolder;
+        _siteService = siteService;
     }
 
     protected override async Task<MenuWidgetViewModel> GetViewModelAsync()
     {
+        var siteSettings = await _siteService.GetSiteSettingsAsync();
+        if (siteSettings.As<BaseThemeSettings>()?.HideMenu == true) return null;
+
         // Add the <nav> classes to the zone holder <nav>.
         _cssClassHolder.AddClassToZone(ZoneNames.Navigation, "navbar-expand-md");
         _cssClassHolder.AddClassToZone(ZoneNames.Navigation, "navbar");
