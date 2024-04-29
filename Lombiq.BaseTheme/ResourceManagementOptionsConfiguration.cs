2f4a84f126343efd3570d05c71f4191d9f4f4811
Aydin Erdas <info@aydinerdas.com>
2024-04-29 13:29:47 +0200

    Use source generator for version

--- Lombiq.BaseTheme/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.BaseTheme/ResourceManagementOptionsConfiguration.cs
@@ -1,20 +1,17 @@
 using Lombiq.BaseTheme.Constants;
+using Lombiq.HelpfulLibraries.Attributes;
 using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
-using System;
 
 namespace Lombiq.BaseTheme;
 
-public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
+[ConstantFromJson("NpmBootstrapVersion", "package.json", "bootstrap")]
+public partial class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
 {
     private const string WwwRoot = "~/" + FeatureIds.Area + "/";
     private const string Css = WwwRoot + "css/";
     private const string Js = WwwRoot + "js/";
     private const string Vendors = WwwRoot + "vendors/";
-
-    // Make sure to keep this exactly the same version as the "bootstrap" dependency in the package.json file.
-    public static readonly Version NpmBootstrapVersion = new(5, 3, 2);
-
     private static readonly ResourceManifest _manifest = new();
 
     static ResourceManagementOptionsConfiguration()
@@ -32,7 +29,7 @@ public class ResourceManagementOptionsConfiguration : IConfigureOptions<Resource
         _manifest
             .DefineScript("bootstrap")
             .SetUrl(Vendors + "bootstrap/js/bootstrap.bundle.min.js", Vendors + "bootstrap/js/bootstrap.bundle.js")
-            .SetVersion(NpmBootstrapVersion.ToString());
+            .SetVersion(NpmBootstrapVersion);
     }
 
     public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
