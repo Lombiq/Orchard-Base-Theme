d0d71b3d6419ac333ddc333b840ec2dba16bbaf8
Sára El-Saig <sara.el-saig@lombiq.com>
2024-09-24 10:49:15 +0200

    Make migrations and background tasks sealed.

--- Lombiq.BaseTheme/Migrations/RecipeMigrations.cs
+++ Lombiq.BaseTheme/Migrations/RecipeMigrations.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Lombiq.BaseTheme.Migrations;
 
-public class RecipeMigrations : DataMigration
+public sealed class RecipeMigrations : DataMigration
 {
     private readonly IRecipeMigrator _recipeMigrator;
 
