c0c744721ee3c0b0e48ddf70bbcd8e5b90a40099
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-19 00:08:59 +0200

    Add Lombiq.BaseTheme.Native.

---
+++ Lombiq.BaseTheme.Native/Manifest.cs
@@ -0,0 +1,14 @@
+using OrchardCore.DisplayManagement.Manifest;
+using static Lombiq.BaseTheme.Core.Constants.FeatureIds;
+
+[assembly: Theme(
+    Name = "Lombiq Base Theme - Native CSS",
+    Author = "Lombiq Technologies",
+    Version = "0.0.1",
+    Website = "https://github.com/Lombiq/Orchard-Base-Theme",
+    Description = "The base frontend theme for shared code that is not specific to a specific project's theme." +
+      "Warning: themes using this as the base remove the stock Bootstrap resource. If you switch to a different " +
+      "theme, please reload the tenant from Configuration → Tenants in the admin menu.",
+    Dependencies = [Core],
+    BaseTheme = Core
+)]
