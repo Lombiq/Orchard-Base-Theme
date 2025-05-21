2220cbb08e5a29d07c0fe0c2657fc1345a26512c
Sára El-Saig <sara.el-saig@lombiq.com>
2025-05-21 19:58:23 +0200

    Add CSS versions of the general theme parts.

--- Lombiq.BaseTheme.Native/Constants/ResourceNames.cs
+++ Lombiq.BaseTheme.Native/Constants/ResourceNames.cs
@@ -2,4 +2,8 @@ namespace Lombiq.BaseTheme.Native.Constants;
 
 public static class ResourceNames
 {
+    private const string Prefix = FeatureIds.Area + ".";
+
+    public const string General = Prefix + nameof(General);
+    public const string GridBreakpoints = Prefix + nameof(GridBreakpoints);
 }
--- Lombiq.BaseTheme.Native/Startup.cs
+++ Lombiq.BaseTheme.Native/Startup.cs
@@ -1,15 +1,24 @@
 using Lombiq.BaseTheme.Native.Constants;
 using Lombiq.HelpfulLibraries.OrchardCore.ResourceManagement;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OrchardCore.Modules;
+using OrchardCore.ResourceManagement;
 
 namespace Lombiq.BaseTheme.Native;
 
 public class Startup : StartupBase
 {
-    public override void ConfigureServices(IServiceCollection services) => services.AddResourceFilter(
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddResourceFilter(
             builder => builder
                 .Always()
-                .RegisterBaseThemeCoreResources(),
+                .RegisterBaseThemeCoreResources()
+                .RegisterStylesheet(ResourceNames.General)
+                .RegisterHeadScript(ResourceNames.GridBreakpoints),
             FeatureIds.Native);
+
+        services.AddTransient<IConfigureOptions<ResourceManagementOptions>, ResourceManagementOptionsConfiguration>();
+    }
 }
